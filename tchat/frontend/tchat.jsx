c9c437224719b378b1241206b38def700cb96a23
Mattie <mattieholtzer@gmail.com>
2018-04-03 16:09:11 -0700

    rendering through a root and app jsx

--- tchat/frontend/tchat.jsx
+++ tchat/frontend/tchat.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {login, signup, logout} from './util/session_api_util'
 import configureStore from './store/store.js'
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -11,5 +12,5 @@ document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore()
   window.getState = store.getState;
   window.dispatch = store.dispatch;
-  ReactDOM.render(<h1>Welcome to Tchat May I take your order please?</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });

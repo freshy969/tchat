e4f2b79321cfe46e3a1cac50e6023bdf8a24a541
Mattie <mattieholtzer@gmail.com>
2018-04-04 09:33:19 -0700

    Logout workign yayayayayay

--- frontend/tchat.jsx
+++ frontend/tchat.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, signup, logout} from './util/session_api_util'
+import {login, signup, logout} from './actions/session_actions'
 import configureStore from './store/store.js'
 import Root from './components/Root';
 

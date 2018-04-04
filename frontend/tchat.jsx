728b79a3d83bf314c483ed3fff8cfe971d822ce7
Mattie <mattieholtzer@gmail.com>
2018-04-03 17:08:42 -0700

    Simple change

--- frontend/tchat.jsx
+++ frontend/tchat.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {login, signup, logout} from './util/session_api_util'
 import configureStore from './store/store.js'
-import Root from './components/root';
+import Root from './components/Root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');

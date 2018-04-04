7c63accbbaab5e90b065e286fdbb61adecb2bd1e
Mattie <mattieholtzer@gmail.com>
2018-04-04 11:04:52 -0700

    Working on bootstrapping current user

--- frontend/tchat.jsx
+++ frontend/tchat.jsx
@@ -5,11 +5,18 @@ import configureStore from './store/store.js'
 import Root from './components/Root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   window.login = login;
   window.logout = logout;
   window.signup = signup;
-  const store = configureStore()
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   ReactDOM.render(<Root store={ store }/>, root);

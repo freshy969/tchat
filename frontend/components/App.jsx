ff10f97052f436213374353ffd5997d145cb1b37
Mattie <mattieholtzer@gmail.com>
2018-04-04 10:10:46 -0700

    Session form getting close

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import WelcomeContainer from './welcome/welcome_container';
+import LoginFormContainer from './login_form/login_form_container'
+import SignupFormContainer from './login_form/signup_form_container'
+import {Route} from 'react-router'
 
 const App = () => (
   <div>
     <h1>Welcome To T-Chat</h1>
     <h2>what's up</h2>
     <WelcomeContainer />
+    <Route path="/login" component={LoginFormContainer} />
+    <Route path="/signup" component={SignupFormContainer} />
   </div>
 );
 

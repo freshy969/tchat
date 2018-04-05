3614dd19544af6926e276e9bfb6d53cffbd36985
Mattie <mattieholtzer@gmail.com>
2018-04-04 23:40:28 -0700

    bus updates

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -18,22 +18,25 @@ const App = () => (
         deserves: better.</p>
       <WelcomeContainer />
       <form className="fakeforms">
-        <div className="fakeform">
-          <select name="sexuality">
-            <option value="valid">Valid</option>
-            <option value="beautiful">Beautiful</option>
-            <option value="fierce">Fierce</option>
-            <option value="witty">Witty</option>
-          </select>
-        </div>
+        <p>You Are A</p>
+        <div className="forms">
+          <div className="fakeform">
+            <select name="sexuality">
+              <option value="valid">Valid</option>
+              <option value="beautiful">Beautiful</option>
+              <option value="fierce">Fierce</option>
+              <option value="witty">Witty</option>
+            </select>
+          </div>
 
-        <div className="fakeform">
-          <select name="gender">
-            <option value="person">Person</option>
-            <option value="person">Person</option>
-            <option value="person">Person</option>
-            <option value="person">Person</option>
-          </select>
+          <div className="fakeform">
+            <select name="gender">
+              <option value="person">Person</option>
+              <option value="person">Person</option>
+              <option value="person">Person</option>
+              <option value="person">Person</option>
+            </select>
+          </div>
         </div>
       </form>
     </header>

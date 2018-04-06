5a4ac82c765d274def72ad35f508f298fded8422
Mattie <mattieholtzer@gmail.com>
2018-04-05 20:09:19 -0700

    Refactoring of things out of app into welcome container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -10,37 +10,7 @@ const App = () => (
   <div>
     <Modal />
     <header>
-      <span className="logo">tChat</span>
-      <h1>YOU DESERVE <br></br> BETTER</h1>
-      <p className="Description">On T-Chat, you’re more than just a gender. You <br></br>
-        have stories to tell, and passions to share, and <br></br>
-        things to talk about that are more interesting than <br></br>
-        the weather. Get noticed for who you are, not <br></br>
-        what you look like. Because you deserve what dating <br></br>
-        deserves: better.</p>
       <WelcomeContainer />
-      <form className="fakeforms">
-        <p className="your">You Are A</p>
-        <div className="forms">
-          <div className="fakeform">
-            <select name="sexuality">
-              <option value="valid">Valid</option>
-              <option value="beautiful">Beautiful</option>
-              <option value="fierce">Fierce</option>
-              <option value="witty">Witty</option>
-            </select>
-          </div>
-
-          <div className="fakeform">
-            <select name="gender">
-              <option value="person">Person</option>
-              <option value="person">Person</option>
-              <option value="person">Person</option>
-              <option value="person">Person</option>
-            </select>
-          </div>
-        </div>
-      </form>
     </header>
     <br></br>
   </div>
--- frontend/components/welcome/welcome.jsx
+++ frontend/components/welcome/welcome.jsx
@@ -8,18 +8,50 @@ import SignupFormContainer from '../login_form/signup_form_container';
 const Welcome = ({ currentUser, logout, openModal }) => {
 
   const sessionLinks = () => (
-    <nav className="login-signup">
-      <div className="login">
-        <span>Have an account?</span>
-        <button className="signin" onClick={() => openModal('login')}>Sign in</button>
+    <div>
+      <span className="logo">tChat</span>
+      <h1>YOU DESERVE <br></br> BETTER</h1>
+      <p className="Description">On T-Chat, you’re more than just a gender. You <br></br>
+        have stories to tell, and passions to share, and <br></br>
+        things to talk about that are more interesting than <br></br>
+        the weather. Get noticed for who you are, not <br></br>
+        what you look like. Because you deserve what dating <br></br>
+        deserves: better.</p>
+      <nav className="login-signup">
+        <div className="login">
+          <span>Have an account?</span>
+          <button className="signin" onClick={() => openModal('login')}>Sign in</button>
+        </div>
+        <div className="signup">
+          <button className="signups" onClick={() => openModal('signup')}>Join Today!</button>
+        </div>
+        <div className="demo-login">
+          <button className="demo-signin" onClick={() => openModal('demo')}>Demo</button>
+        </div>
+      </nav>
+      <form className="fakeforms">
+        <p className="your">You Are A</p>
+        <div className="forms">
+          <div className="fakeform">
+            <select name="sexuality">
+              <option value="valid">Valid</option>
+              <option value="beautiful">Beautiful</option>
+              <option value="fierce">Fierce</option>
+              <option value="witty">Witty</option>
+            </select>
+          </div>
+
+        <div className="fakeform">
+          <select name="gender">
+            <option value="person">Person</option>
+            <option value="person">Person</option>
+            <option value="person">Person</option>
+            <option value="person">Person</option>
+          </select>
+        </div>
       </div>
-      <div className="signup">
-        <button className="signups" onClick={() => openModal('signup')}>Join Today!</button>
-      </div>
-      <div className="demo-login">
-        <button className="demo-signin" onClick={() => openModal('demo')}>Demo</button>
-      </div>
-    </nav>
+    </form>
+  </div>
   );
 
   const personalWelcome = () => (

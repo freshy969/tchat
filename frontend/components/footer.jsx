c5671a59882f7280a21c644b845c609bc84ca031
Mattie <mattieholtzer@gmail.com>
2018-04-06 09:44:34 -0700

    working in localhost lol

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer>
-    <a href="https://www.linkedin.com/in/mattieholtzer/">
-      <img className="linkedin" src="assets/linkedin-logo.png"/>
-    </a>
-    <a href="https://github.com/mattieholtzer">
-      <img className="linkedin" src="assets/github-logo.png"/>
-    </a>
+  <footer className="footer">
+    <div className="footer-content">
+      <div className="upper-footer">
+        <p>
+          A project by Mattie Holtzer
+        </p>
+      </div>
+      <div className="lower-footer">
+        <a href="https://www.linkedin.com/in/mattieholtzer/" target="_blank" >
+          <img className="linkedin" src="assets/linkedin-logo.png"/>
+        </a>
+        <a href="https://github.com/mattieholtzer/tchat" target="_blank">
+          <img className="linkedin" src="assets/github-logo.png"/>
+        </a>
+      </div>
+    </div>
   </footer>
 )
 

a086a1560fd81bd885ff28fe9eae81b6395deee8
Mattie <mattieholtzer@gmail.com>
2018-04-06 22:18:47 -0700

    getting somewhere

--- frontend/components/user/my_profile.jsx
+++ frontend/components/user/my_profile.jsx
@@ -18,13 +18,19 @@ class Profile extends React.Component {
     return(
       <header className="profile-container">
         <div className="profile">
-          <img className="profpicture" src={`assets/${this.props.currentUser.img_url}`}/>
-          {this.props.currentUser.screen_name}
-          {this.props.currentUser.city}
-          {this.props.currentUser.pronouns}
-          {this.props.currentUser.age}
-          {this.props.currentUser.bio}
-          {this.props.currentUser.first_date_idea}
+          <div className="top-bar">
+            <img className="profpicture" src={`assets/${this.props.currentUser.img_url}`}/>
+            <div className="top-info">
+              <span className="screen-name">{this.props.currentUser.screen_name}</span>
+              <div className="top-sub-info">
+                <span className="age">{this.props.currentUser.age}</span>
+                <span className="city">{this.props.currentUser.city}</span>
+                <span className="pronouns">{this.props.currentUser.pronouns}</span>
+              </div>
+            </div>
+          </div>
+          <span className="bio">{this.props.currentUser.bio}</span>
+          <span className="first-date-idea">{this.props.currentUser.first_date_idea}</span>
         </div>
       </header>
     )

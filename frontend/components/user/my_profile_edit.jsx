fc2b2f2bd92a9ac4b76ce7a0187d63ef80a2a5ba
Mattie <mattieholtzer@gmail.com>
2018-04-09 16:56:25 -0700

    starting to make hard moves on edit everything still working

--- frontend/components/user/my_profile_edit.jsx
+++ frontend/components/user/my_profile_edit.jsx
@@ -14,26 +14,29 @@ class ProfileEdit extends React.Component {
       first_date_idea: this.props.first_date_idea,
       my_aesthetic: this.props.my_aesthetic,
       my_anthem: this.props.my_anthem,
+      hobbies: this.props.hobbies,
       looking_for: this.props.looking_for
     };
   }
 
   render() {
-
+    console.log(this.props)
     return(
       <header className="profile-container">
         <div className="profile">
           <div className="top-bar">
-            <img className="profpicture" src={`assets/${this.props.currentUser.img_url || 'generic.jpg'}`}/>
+            <img className="profpicture" src={`assets/${this.props.img_url || 'generic.jpg'}`}/>
             <div className="top-info">
-              <span className="screen-name">{this.props.currentUser.screen_name}</span>
+              <span className="screen-name">
+                {this.props.screen_name}
+              </span>
               <div className="top-sub-container">
                 <div className="top-sub-info">
-                  <span className="age">{this.props.currentUser.age}</span>
+                  <span className="age">{this.props.age}</span>
                   <span className="drama-dot">.</span>
-                  <span className="city">{this.props.currentUser.city}</span>
+                  <span className="city">{this.props.city}</span>
                   <span className="drama-dot">.</span>
-                  <span className="pronouns">{this.props.currentUser.pronouns}</span>
+                  <span className="pronouns">{this.props.pronouns}</span>
                 </div>
                 <div className="profile-nav-buttons-container">
                   <Link to="/profile" className="profile-nav-buttons">
@@ -47,22 +50,22 @@ class ProfileEdit extends React.Component {
           <div className="bottom">
             <div className="bottom-left">
               <h2 className="questions">Bio</h2>
-              <p className="answers">{this.props.currentUser.bio || '-'}</p>
+              <p className="answers">{this.props.bio || '-'}</p>
               <h2 className="questions">First date idea</h2>
-              <p className="answers">{this.props.currentUser.first_date_idea || '-'}</p>
+              <p className="answers">{this.props.first_date_idea || '-'}</p>
             </div>
             <div className="bottom-right">
               <div className="bright-container">
                 <img className="bright-images bwhite" src="assets/painting-nails.png"/>
-                <p className="bright-answers">{this.props.currentUser.my_aesthetic || '-'}</p>
+                <p className="bright-answers">{this.props.my_aesthetic || '-'}</p>
               </div>
               <div className="bright-container">
                 <img className="bright-images" src="assets/music-stock.png"/>
-                <p className="bright-answers">{this.props.currentUser.my_anthem || '-'}</p>
+                <p className="bright-answers">{this.props.my_anthem || '-'}</p>
               </div>
               <div className="bright-container">
                 <img className="bright-images" src="assets/hobbies.png"/>
-                <p className="bright-answers">{this.props.currentUser.hobbies || '-'}</p>
+                <p className="bright-answers">{this.props.hobbies || '-'}</p>
               </div>
               <div className="looking-for">
                 <p className="bright-answers"><span className="boldedgirl">Looking For </span>{this.props.currentUser.looking_for || '-'}</p>

74afa0237dc7d54893ec9b5bb47af9e0c69eb292
Mattie <mattieholtzer@gmail.com>
2018-04-09 22:02:02 -0700

    okay now edit fields showing up need to figure out how to submit them

--- frontend/components/user/my_profile_edit.jsx
+++ frontend/components/user/my_profile_edit.jsx
@@ -5,17 +5,17 @@ class ProfileEdit extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      img_url: this.props.user.img_url,
-      screen_name: '',
-      age: '',
-      city: '',
-      pronouns: '',
-      bio: '',
-      first_date_idea: '',
-      my_aesthetic: '',
-      my_anthem: '',
-      hobbies: '',
-      looking_for: ''
+      id: this.props.id,
+      screen_name: this.props.screen_name,
+      age: this.props.age,
+      city: this.props.city,
+      pronouns: this.props.pronouns,
+      bio: this.props.bio,
+      first_date_idea: this.props.first_date_idea,
+      my_aesthetic: this.props.my_aesthetic,
+      my_anthem: this.props.my_anthem,
+      hobbies: this.props.hobbies,
+      looking_for: this.props.looking_for
     };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -43,18 +43,36 @@ class ProfileEdit extends React.Component {
             <div className="top-info">
               <span className="screen-name">
                 <input type="string"
-                  value={this.props.screen_name? this.props.screen_name : this.state.screen_name}
-                  onChange={this.update('age')}
+                  value={this.state.screen_name}
+                  onChange={this.update('screen_name')}
                   className="login-input"
                 />
               </span>
               <div className="top-sub-container">
                 <div className="top-sub-info">
-                  <span className="age">{this.state.age}</span>
+                  <span className="age">
+                    <input type="number"
+                      value={this.state.age}
+                      onChange={this.update('age')}
+                      className="login-input"
+                      />
+                  </span>
                   <span className="drama-dot">.</span>
-                  <span className="city">{this.state.city}</span>
+                  <span className="city">
+                    <input type="string"
+                      value={this.state.city}
+                      onChange={this.update('city')}
+                      className="login-input"
+                      />
+                  </span>
                   <span className="drama-dot">.</span>
-                  <span className="pronouns">{this.state.pronouns}</span>
+                  <span className="pronouns">
+                    <input type="string"
+                      value={this.state.pronouns}
+                      onChange={this.update('pronouns')}
+                      className="login-input"
+                      />
+                  </span>
                 </div>
                 <div className="profile-nav-buttons-container">
                   <input type="submit" value="Update" />
@@ -66,25 +84,61 @@ class ProfileEdit extends React.Component {
           <div className="bottom">
             <div className="bottom-left">
               <h2 className="questions">Bio</h2>
-              <p className="answers">{this.state.bio || '-'}</p>
+              <p className="answers">
+                <input type="string"
+                  value={this.state.bio}
+                  onChange={this.update('bio')}
+                  className="login-input"
+                  />
+              </p>
               <h2 className="questions">First date idea</h2>
-              <p className="answers">{this.state.first_date_idea || '-'}</p>
+              <p className="answers">
+                <input type="string"
+                  value={this.state.first_date_idea}
+                  onChange={this.update('first_date_idea')}
+                  className="login-input"
+                  />
+              </p>
             </div>
             <div className="bottom-right">
               <div className="bright-container">
                 <img className="bright-images bwhite" src="assets/painting-nails.png"/>
-                <p className="bright-answers">{this.state.my_aesthetic || '-'}</p>
+                <p className="bright-answers">
+                  <input type="string"
+                    value={this.state.my_aesthetic}
+                    onChange={this.update('my_aesthetic')}
+                    className="login-input"
+                    />
+                </p>
               </div>
               <div className="bright-container">
                 <img className="bright-images" src="assets/music-stock.png"/>
-                <p className="bright-answers">{this.state.my_anthem || '-'}</p>
+                <p className="bright-answers">
+                  <input type="string"
+                    value={this.state.my_anthem}
+                    onChange={this.update('my_anthem')}
+                    className="login-input"
+                    />
+                </p>
               </div>
               <div className="bright-container">
                 <img className="bright-images" src="assets/hobbies.png"/>
-                <p className="bright-answers">{this.state.hobbies || '-'}</p>
+                <p className="bright-answers">
+                  <input type="string"
+                    value={this.state.hobbies}
+                    onChange={this.update('hobbies')}
+                    className="login-input"
+                    />
+                </p>
               </div>
               <div className="looking-for">
-                <p className="bright-answers"><span className="boldedgirl">Looking For </span>{this.state.looking_for || '-'}</p>
+                <p className="bright-answers"><span className="boldedgirl">Looking For </span>
+                  <input type="string"
+                    value={this.state.looking_for}
+                    onChange={this.update('looking_for')}
+                    className="login-input"
+                    />
+                </p>
               </div>
             </div>
           </div>
--- frontend/components/user/my_profile_edit_container.jsx
+++ frontend/components/user/my_profile_edit_container.jsx
@@ -6,17 +6,18 @@ import ProfileEdit from './my_profile_edit';
 
 const mapStateToProps = (state) => {
   return {
+    id: state.session.currentUser.id,
     img_url: '',
-    screen_name: '',
-    age: '',
-    city: '',
-    pronouns: '',
-    bio: '',
-    first_date_idea: '',
-    my_aesthetic: '',
-    my_anthem: '',
-    hobbies: '',
-    looking_for: ''
+    screen_name: state.session.currentUser.screen_name,
+    age: state.session.currentUser.age,
+    city: state.session.currentUser.city,
+    pronouns: state.session.currentUser.pronouns,
+    bio: state.session.currentUser.bio,
+    first_date_idea: state.session.currentUser.first_date_idea,
+    my_aesthetic: state.session.currentUser.my_aesthetic,
+    my_anthem: state.session.currentUser.my_anthem,
+    hobbies: state.session.currentUser.hobbies,
+    looking_for: state.session.currentUser.looking_for
   };
 };
 

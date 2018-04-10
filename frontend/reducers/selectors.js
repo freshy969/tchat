006d0695af364afd48ff68f49f7eb1ef198de96d
Mattie <mattieholtzer@gmail.com>
2018-04-10 15:37:03 -0700

    filtering by location yayayayayayayay

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,3 +1,15 @@
 import values from 'lodash/values';
 
-export const selectUsers = state => values(state.users);
+// export const selectUsers = state => values(state.users);
+
+export const selectUsers = state => {
+  let result = [];
+  for (let id in state.users){
+    if (state.users[id].id !== state.session.currentUser.id){
+      if (state.users[id].city === state.session.currentUser.city){
+        result.push(state.users[id]);
+      }
+    }
+  }
+  return result;
+};

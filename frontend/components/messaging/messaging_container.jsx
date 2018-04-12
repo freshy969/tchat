f00761085aa30190b8b06c21b15a53ac9cdecc84
Mattie <mattieholtzer@gmail.com>
2018-04-12 14:39:06 -0700

    things not broken yet

--- frontend/components/messaging/messaging_container.jsx
+++ frontend/components/messaging/messaging_container.jsx
@@ -2,14 +2,19 @@ import { connect } from 'react-redux';
 import React from 'react';
 import Messaging from './messaging';
 import { selectMessages, selectMessagethreads } from '../../reducers/selectors';
-import { requestMessages, requestMessagethreads} from '../../actions/message_actions';
+import { requestMessages, requestMessagethreads, postNewMessage} from '../../actions/message_actions';
 import {receiveCurrentMessagethreadId} from '../../actions/message_actions';
 
 const mapStateToProps = (state, ownProps) => ({
   messages: selectMessages(state),
   messagethreads: selectMessagethreads(state),
   currentUser: state.session.currentUser,
-  currentMessagethread: state.messagethreads[state.ui.currentMessagethread]
+  currentMessagethread: state.messagethreads[state.ui.currentMessagethread],
+  message_thread_id: state.ui.currentMessagethread,
+  sender_id: state.session.currentUser.id,
+  message: "",
+  receiver_id: state.ui.currentMessagethread===-1? -1: (state.session.currentUser.id === state.messagethreads[state.ui.currentMessagethread].initiator_id? state.messagethreads[state.ui.currentMessagethread].receiver_id : state.messagethreads[state.ui.currentMessagethread].initiator_id),
+
 });
 
 const mapDispatchToProps = dispatch => {
